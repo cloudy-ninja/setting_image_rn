c37145ab4bd2e123a47d539feb77108dbc545ea6
DenisDovganiuc <denis.dev.0324@gmail.com>
2019-07-08 16:19:32 -0700

    add onChooseImage func

--- screen/Home.js
+++ screen/Home.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Alert } from 'react-native'
+import ImagePicker from 'react-native-image-picker';
 import {
   ColorButton,
   HomeContainer,
 } from '../components/Home'
 
 export class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onChooseImage = () => {
+    const options = {
+      title: 'Select Your Photo',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      },
+      quality: 0.3
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+      } else if (response.error) {
+        Alert.alert(response.error);
+      } else {
+      }
+    });
+  }
+
   render() {
     return (
       <HomeContainer>
         <ColorButton
           title={'Choose Image'}
+          onPress={this.onChooseImage}
         />
       </HomeContainer>
     );
